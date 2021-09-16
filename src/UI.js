2e94358b9475a21263c3c6b058c85947b51d0ccc
usman <tasayev.usman@gmail.com>
2021-09-16 17:06:04 -0400

    createlogic for the project tab to append to the projects container

--- src/UI.js
+++ src/UI.js
@@ -13,6 +13,7 @@ function loadEvents(){
   });
   projectNameInput.parentElement.addEventListener('submit', (e) => {
     data.newProject(projectNameInput.value);
+    createProjectTab(projectNameInput.value);
     clearInput(projectNameInput);
     //loadProject(projectName.value);
     projectNameInput.classList.toggle('hidden');
@@ -24,6 +25,14 @@ function loadEvents(){
 
 function clearInput(inputs) {
   inputs.value = '';
+}
+
+function createProjectTab(projectName) {
+  const tabContainer = document.querySelector('#projects-tab-container')
+  const tab = document.createElement('button');
+  tab.textContent = projectName;
+  tabContainer.prepend(tab);
+  //add the option to delete this tab, also it should be a div rather than a button
 }
 
 export { loadPage }
